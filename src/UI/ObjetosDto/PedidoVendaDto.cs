a21a4abcf7ac4dc205240e0ed68e11458445f590
Gabriel Trucollo <gabrieltrucollo@gmail.com>
2022-06-22 11:41:16 -0300

    chore: consumir a api do backend

---
+++ src/UI/ObjetosDto/PedidoVendaDto.cs
@@ -0,0 +1,20 @@
+﻿namespace UI.ObjetosDto
+{
+    public class PedidoVendaDto
+    {
+        /// <summary>
+        /// Obtém ou define ValorTotal
+        /// </summary>
+        public double ValorTotal { get; set; }
+
+        /// <summary>
+        /// Obtém ou define Quantidade
+        /// </summary>
+        public uint Quantidade { get; set; }
+
+        /// <summary>
+        /// Obtém ou define Items
+        /// </summary>
+        public List<PedidoVendaItemDto> Items { get; set; } = new();
+    }
+}
---
+++ src/UI/ObjetosDto/ProdutoDto.cs
@@ -0,0 +1,15 @@
+﻿namespace UI.ObjetosDto
+{
+    public class ProdutoDto
+    {
+        /// <summary>
+        /// Obtém ou define Nome
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Obtém ou define Valor
+        /// </summary>
+        public double Valor { get; set; }
+    }
+}
